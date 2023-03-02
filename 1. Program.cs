1ab85c631d804574968584975272d84ee4476b67
Sahdev Saini <59026147+sahdevsaini@users.noreply.github.com>
2023-03-02 22:52:58 +0530

    Add files via upload

---
+++ 1. Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+ {
+        static void Main()
+       {
+        Console.WriteLine("This is first program");
+        Console.ReadKey();
+        Main1();
+        }
+    static void Main1()
+    {
+        Console.WriteLine("This is first program");
+        Console.ReadKey();
+    }
+
+  }
+
