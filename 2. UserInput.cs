c2553f302194bb7ba5683bcf565d1570f09c4cf2
Sahdev Saini <59026147+sahdevsaini@users.noreply.github.com>
2023-03-04 19:35:40 +0530

    Add files via upload

---
+++ 2. UserInput.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class UserInput
+{
+    static void Main()
+    {
+
+        Console.WriteLine("Enter the input: ");
+        string Firstname = Console.ReadLine();
+        Console.WriteLine("Enter the input: ");
+        string Lastname = Console.ReadLine();
+        Console.WriteLine("Hello {0}, {1}",  Firstname,Lastname);
+        Console.ReadKey();
+
+        //Console.WriteLine("Enter the input: ");
+        //string username = Console.ReadLine();
+        //Console.WriteLine("Hello " + username);
+        //Console.ReadKey();
+    }
+}
