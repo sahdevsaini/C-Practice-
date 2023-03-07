bbac261210572d76aa87e17614c9329e40ac7c47
Sahdev Saini <59026147+sahdevsaini@users.noreply.github.com>
2023-03-07 20:46:44 +0530

    Add files via upload

---
+++ 4. Built in string types .cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Datatypes
+{
+    static void Main()
+    {
+        //string name = "Sahdev Saini";
+        /* string name = "\"Sahdev Saini\"";*/     // o/p = "Sahdev Saini"
+                                                   /*string name = "\\Sahdev Saini\\"; */         // O/p = \Sahdev Saini\
+                                                                                                  /*string name = "List of Students\nSahdev Saini\nGansha Kumar\nRahul kumar";*/    /* O/P = Sahdev Saini
+                                                                                                                                                                                         Gansha Kumar
+                                                                                                   Rahul Kumar*/
+        string name = "D:\\Motherson\\MTSL\\iGHS CIS\\G CIRES";
+        Console.WriteLine(name);
+        Console.ReadKey();
+
+    }
+}
