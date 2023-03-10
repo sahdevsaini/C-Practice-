2d0376e8698c9fef739aad0b2d0de04fc35c2268
Sahdev Saini <59026147+sahdevsaini@users.noreply.github.com>
2023-03-10 23:34:45 +0530

    Add files via upload

---
+++ 5. Nullable types in C#.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class Datatypes
+{
+    static void Main()
+    {
+        //bool? IamMajor = null;
+        //bool? IamMajor = true;
+        bool? IamMajor = false;
+        if (IamMajor == true)
+        {
+            Console.WriteLine("User is Major");
+        }
+        else if (IamMajor == false)
+        {
+            Console.WriteLine("User is not major");
+        }
+        else
+        {
+            Console.WriteLine("User didn't answer the question");
+        }
+        Console.ReadKey();
+
+    }
+}
