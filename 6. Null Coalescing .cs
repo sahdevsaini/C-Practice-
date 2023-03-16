86acf9e561ab5633eeb8569d4c5b001e332575e4
Sahdev Saini <59026147+sahdevsaini@users.noreply.github.com>
2023-03-16 23:12:59 +0530

    Add files via upload

---
+++ 6. Null Coalescing .cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class Datatypes
+{
+    static void Main()
+    {
+        int ? ticketonsale = 100;
+        int ticketavailable= ticketonsale ?? 0;
+
+        //if (ticketonsale == 0)
+        //{
+        //    ticketavailable = 0;
+        //}
+        //else
+        //{
+        //    ticketavailable = (int) ticketonsale;
+        //}
+        Console.WriteLine("ticketavailable {0}", ticketavailable);
+        Console.ReadKey();
+
+    }
+}
