5c3406f0e10820eb32328c54ce33b11c904ebf9f
Sahdev Saini <59026147+sahdevsaini@users.noreply.github.com>
2023-03-18 17:58:11 +0530

    Add files via upload

---
+++ 7 Data type Conversion .cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public class Datatypes
+{
+    static void Main()
+    {
+        //int n = 45;
+        //float f = 123.45F;
+        //int i = (int)f;
+        //int i = Convert.ToInt32(f);
+        //int j = Convert.ToInt16(f);
+        //decimal k = Convert.ToDecimal(n);
+        //Console.WriteLine(i);
+        //Console.WriteLine(j);
+        //Console.WriteLine(k);
+        //float strname =108.7F;
+        ////int i = int.Parse(strname);
+        //Console.WriteLine(i);
+        bool res;
+        int a;
+        string myStr = "12";
+        res = int.TryParse(myStr, out a);
+        Console.WriteLine(res);
+        Console.ReadKey();
+
+    }
+}
