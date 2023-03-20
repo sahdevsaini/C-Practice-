88981c2fce8bb03758f6d30ef7cc5f2be99705eb
Sahdev Saini <59026147+sahdevsaini@users.noreply.github.com>
2023-03-20 08:01:45 +0530

    Add files via upload

---
+++ 10. Switch Case.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Datatypes
+{
+    static void Main()
+    {
+        //int b = 0;
+        //Console.WriteLine("int ={0}", int.MinValue);
+        //Console.WriteLine("int ={0}", int.MaxValue);
+        //Console.ReadKey();
+
+        double d = 123.2234;
+        Console.WriteLine(d);
+        Console.ReadKey();
+
+    }
+}
