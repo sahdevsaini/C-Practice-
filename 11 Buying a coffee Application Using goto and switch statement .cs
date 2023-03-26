0ad5374272cd915853c6a1ee745dca9f2ac00202
Sahdev Saini <59026147+sahdevsaini@users.noreply.github.com>
2023-03-26 23:10:29 +0530

    Add files via upload

---
+++ 11 Buying a coffee Application Using goto and switch statement .cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+public class Array
+{
+    static void Main()
+    {
+        int TotalCoffeCost = 0;
+    Start:
+        Console.WriteLine("1. Samll  2.Medium  3.Large");
+        int userchoice = int.Parse(Console.ReadLine());
+        switch (userchoice)
+        {
+            case 1:
+                TotalCoffeCost += 20;
+                break;
+            case 2:
+                TotalCoffeCost += 40;
+                break;
+            case 3:
+                TotalCoffeCost += 50;
+                break;
+            default:
+                goto Start;
+        }
+
+        Console.WriteLine("Total bill {0}", TotalCoffeCost);
+
+    Decision:
+        Console.WriteLine("Do you want to buy another coffee 1. Yes 2. No");
+        String userdecision = Console.ReadLine();
+        switch (userdecision.ToUpper())
+        {
+            case "YES":
+                goto Start;
+
+            case "NO":
+                break;
+            default:
+                goto Decision;
+        }
+        Console.WriteLine("Thanks for shopping");
+        Console.ReadKey();
+    }
+
+}
