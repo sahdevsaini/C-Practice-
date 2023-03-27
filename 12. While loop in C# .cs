2f2fdd3f17df752809fcf519c270e305c6f587de
Sahdev Saini <59026147+sahdevsaini@users.noreply.github.com>
2023-03-27 20:54:04 +0530

    Add files via upload

---
+++ 12. While loop in C# .cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class Loop
+{
+
+    static void Main()
+    {
+        Console.WriteLine("Enter the Target Value : ");
+        int targetvalue = int.Parse(Console.ReadLine());
+        int start = 0;
+        while (start <= targetvalue)
+        {
+            Console.WriteLine(start);
+            start += 2;
+        }
+
+        Console.ReadKey();
+    }
+
+}
