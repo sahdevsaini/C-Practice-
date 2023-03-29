73e1d75ea91be35e05e44881e4998cbf4f2e00f7
Sahdev Saini <59026147+sahdevsaini@users.noreply.github.com>
2023-03-29 20:01:22 +0530

    Add files via upload

---
+++ 14. For and Foreach Loop.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+public class Loop
+{
+
+    static void Main()
+    {
+        //int[] num = new int[5] { 3, 4, 5, 6, 8 };
+        //for(int i =0; i<=num.Length; i++)
+        //{
+        //    Console.WriteLine(i);
+        //}
+
+        //foreach(int k in num)
+        //{
+        //    Console.WriteLine(k);
+        //}
+        //Console.ReadKey();
+
+        //int j = 0;
+        //while(j<num.Length)
+        //{
+        //    Console.WriteLine(j);
+        //    j++;
+        // }
+
+        // program to print Even numbers
+
+        //for (int i=0; i<=20;i++)
+        //{
+        //    if (i % 2 == 0)
+        //        continue;
+        //    Console.WriteLine(i);
+        //}
+        //Console.ReadKey();
+
+        for (int i = 0; i <= 20; i++)
+        {
+            if (i % 2 == 1)
+                Console.WriteLine(i);
+        }
+        Console.ReadKey();
+
+    }
+}
