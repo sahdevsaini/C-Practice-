e1a022faa5276d93e83edc74feb4cde3bd437554
Sahdev Saini <59026147+sahdevsaini@users.noreply.github.com>
2023-04-01 08:13:49 +0530

    Add files via upload

---
+++ 15. Methods .cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+public class program
+{
+
+    static void Main()
+    {
+        Console.WriteLine("Enter the number");
+        int n = int.Parse(Console.ReadLine());
+        program.EvenNumber(n);
+        program p1 = new program();
+        p1.oddnumber(n);
+        program p = new program();
+        Console.WriteLine("\nThe Sum of two number is :- {0}", p.add1(10, 20));
+        Console.ReadKey();
+
+    }
+    public void oddnumber(int n)
+    {
+        if (n % 2 != 0)
+        {
+            Console.WriteLine("Given number is Odd");
+        }
+        else
+        {
+            Console.WriteLine("Given Number is Even");
+        }
+
+    }
+
+    public int add1(int a, int b)
+    {
+        return a + b;
+    }
+    public static void EvenNumber(int target)
+    {
+        int start = 0;
+        while (start < target)
+        {
+            Console.WriteLine(start);
+            start += 2;
+        }
+    }
+}
