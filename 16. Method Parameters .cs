abb74cc65468f6aca98fe6282782ada1061e7a1f
Sahdev Saini <59026147+sahdevsaini@users.noreply.github.com>
2023-04-02 21:28:07 +0530

    Add files via upload

---
+++ 16. Method Parameters .cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public class ParameterClass
+{
+
+    //public static void Main()
+    // {
+    //     int i = 0;
+    //     ParameterClass.parameter(i);
+    //     Console.WriteLine(i);
+    //     Console.ReadKey();
+    // }
+    // public static void parameter(int j)
+    // {
+    //     j = 101;
+
+    // }
+
+    public static void Main()
+    {
+        int i = 0;
+        ParameterClass.parameter(ref i);
+        Console.WriteLine(i);
+        Console.ReadKey();
+    }
+    public static void parameter(ref int j)
+    {
+        j = 101;
+
+    }
+}
