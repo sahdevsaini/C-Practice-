d5ba8a872d1a073d8a29c8faa6ed5bf27fb43fba
Sahdev Saini <59026147+sahdevsaini@users.noreply.github.com>
2023-04-04 20:44:00 +0530

    Add files via upload

---
+++ 18. Array parameter use of params keyword .cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class ParameterClass
+{
+    public static void Main()
+    {
+        int[] newlist = new int[5] { 10, 20, 5, 8, 50 };
+        ArrayParameter(newlist);
+        Console.Read();
+    }
+    public static void ArrayParameter(params int[] newlist)
+    {
+        Console.WriteLine("There are {0} elements in array", newlist.Length);
+        foreach (int i in newlist)
+        {
+            Console.WriteLine(i);
+        }
+
+    }
+
+}
