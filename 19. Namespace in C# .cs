f89fb7553b6166f949f768815524dda4c986995e
Sahdev Saini <59026147+sahdevsaini@users.noreply.github.com>
2023-04-07 21:21:49 +0530

    Add files via upload

---
+++ 19. Namespace in C# .cs
@@ -0,0 +1,42 @@
+﻿using System;
+using ProgrameA.TeamA;
+using ProgrameA.TeamB;
+
+public class name
+{
+    public static void Main()
+    {
+        A.Print();
+        B.Print();
+        Console.ReadKey();
+    }
+}
+namespace ProgrameA
+{
+    namespace TeamA
+    {
+        public class A
+        {
+            public static void Print()
+            {
+                Console.WriteLine("This is Team A");
+            }
+        }
+    }
+}
+
+namespace ProgrameA
+{
+    namespace TeamB
+    {
+        public class B
+        {
+            public static void Print()
+            {
+                Console.WriteLine("This is Team B");
+            }
+        }
+    }
+}
+
+
