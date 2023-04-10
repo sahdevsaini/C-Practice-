cae110e18712bc6a9917db98b4a0aa4ed9285735
Sahdev Saini <59026147+sahdevsaini@users.noreply.github.com>
2023-04-10 20:43:06 +0530

    Add files via upload

---
+++ 20. Alias of Namespace in C# .cs
@@ -0,0 +1,42 @@
+﻿using System;
+using PATA = ProgrameA.TeamA;    // PATA is the alias of Namesapce programe A and Team A. 
+using PATB = ProgrameA.TeamB;    // PATB is the alias of Namesapce programe A and Team B.
+
+public class name
+{
+    public static void Main()
+    {
+        PATA.A.Print();         // Calling of Print function using Alias.
+        PATB.B.Print();         // Calling of Print function using Alias.
+        Console.ReadKey();
+    }
+}
+namespace ProgrameA
+{
+    namespace TeamA
+    {
+        public class A
+        {
+            public static void Print()
+            {
+                Console.WriteLine("This is Team A");
+            }
+        }
+    }
+}
+
+namespace ProgrameA
+{
+    namespace TeamB
+    {
+        public class B
+        {
+            public static void Print()
+            {
+                Console.WriteLine("This is Team B");
+            }
+        }
+    }
+}
+
+
